90649fa83ff15f896086a130a60eac6571283913
Ben Oliver <benjamin.e.oliver@gmail.com>
2018-07-22 11:22:36 +0200

    added project info

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject insert-randomly "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject beoliver/insert-randomly "0.1.0-SNAPSHOT"
+  :description "spice up your data structures"
+  :url "https://github.com/beoliver/insert-randomly"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]])
